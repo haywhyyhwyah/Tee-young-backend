b9e4e80b6b0a5d542e28f9130dd3d77905604e75
haywhyyhwyah <ayomideisrael623@gmail.com>
2026-04-04 00:14:38 +0100

    Update CORS origin to allow requests from the production URL

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://tee-young-autos.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }));
